a9dddf7b1996419504a1b4a98114f71d6d25808e
Catherine Kidner <ckidner@vm142-66.cyverse.org>
2018-10-04 05:11:26 -0700

    modified raw_to_fasta to use missing rather than reference

--- raw_to_fasta_BWA.sh
+++ raw_to_fasta_BWA.sh
@@ -18,6 +18,7 @@ rc=${acc}_rc.txt
 vcf=${acc}.vcf.gz
 Q_vcf=${acc}_qual.vcf.gz
 bwa=${acc}_bwa.bam
+bwa_report=${acc}_bwa_report
 
 echo "You're working on accession $1"
 
@@ -25,12 +26,12 @@ echo "You're working on accession $1"
 java -jar ~/../../opt/Trimmomatic-0.36/trimmomatic-0.36.jar PE -phred33 $F $R forward_paired.fq.gz forward_unpaired.fq.gz reverse_paired.fq.gz reverse_unpaired.fq.gz ILLUMINACLIP:~/../../opt/Trimmomatic-0.36/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
 
 # BWA
-bwa mem Baits.fna forward_paired.fq.gz reverse_paired.fq.gz > output.sam 2> $bwa
+bwa mem Baits.fna forward_paired.fq.gz reverse_paired.fq.gz > output.sam 2> $bwa_report
 
 rm *fq.gz
 
 #to sorted, de-duplicated bam
-samtools view -bu output.sam | samtools sort -n -T /tmp - | samtools fixmate -mr - - | samtools sort -T /tmp -| samtools markdup -sr - ~Process/Hairdrier/$bwa
+samtools view -bu output.sam | samtools sort -n -T /tmp - | samtools fixmate -mr - - | samtools sort -T /tmp -| samtools markdup -sr - ~/Process/Hairdrier/$bwa
 
 #run without de-dup
 #samtools view -bu output.sam | samtools sort -n -T /tmp - | samtools fixmate -mr - - | samtools sort - ~/Process/Hairdrier/$bwa
@@ -39,7 +40,7 @@ rm output.sam
 
 # call 
 
-bcftools mpileup -B -Ou -f Baits.fna ~/Process/Hairdrier/$bwa  | bcftools call -mv -Ou | bcftools filter -S 0 -i 'FMT/GT="1/1" & QUAL > 29' -Oz -o $Q_vcf
+bcftools mpileup -B -Ou -f Baits.fna ~/Process/Hairdrier/$bwa  | bcftools call -mv -Ou | bcftools filter -S . -i 'FMT/GT="1/1" & QUAL > 29' -Oz -o $Q_vcf
 
 tabix $Q_vcf
 
